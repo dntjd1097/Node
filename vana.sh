cd428edf8bb15599b9a8e890fe3e0f2e36c2ddfe
dntjd1097 <dntjd1097@gmail.com>
2024-12-04 21:03:31 +0900

    expect dependencies

--- vana.sh
+++ vana.sh
@@ -2,6 +2,7 @@
 
 install_dependencies() {
     sudo apt update && sudo apt install software-properties-common -y 
+	sudo apt-get install expect -y
 	sudo add-apt-repository ppa:deadsnakes/ppa
 	sudo apt update
 	sudo apt install python3.11 python3.11-venv python3.11-dev -y
